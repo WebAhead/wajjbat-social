756116028ea4ec94bb3f8e67b47fc5b53bc56e31
Francis Aboud <francisaboud2000@gmail.com>
2020-03-26 12:45:23 +0200

    ads comment section

---
+++ src/components/CommentSection/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Button, Comment, Form, Header, Card } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import comments from "./comments.json";
+export default function CommentSection() {
+  const [collapsed, setCollapsed] = React.useState(true);
+
+  const handleCollapsed = e => {
+    setCollapsed(!collapsed);
+    e.target.textContent = collapsed ? "hide comments" : "show all comments";
+  };
+
+  return (
+    <Comment.Group style={{ padding: 10, margin: 20 }}>
+      <Header as="h3" dividing>
+        Comments
+      </Header>
+      <a onClick={handleCollapsed}>see all comments</a>
+
+      {comments.map((comment, i) => (
+        <Comment collapsed={comment.top ? "" :  collapsed }>
+          <Comment.Avatar src={comment.avatar} />
+          <Comment.Content>
+            <Comment.Author as="a">
+              {comment.author}{" "}
+              <Comment.Metadata>
+                <div>{comment.time}</div>
+              </Comment.Metadata>
+            </Comment.Author>
+            <Comment.Text>{comment.text}</Comment.Text>
+          </Comment.Content>
+        </Comment>
+      ))}
+
+      <Form reply>
+        <Form.TextArea />
+        <Button content="Add Reply" labelPosition="left" icon="edit" primary />
+      </Form>
+    </Comment.Group>
+  );
+}
--- src/pages/ViewPost/index.jsx
+++ src/pages/ViewPost/index.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import ListField from '../../components/ListField'
+import React from "react";
+import ListField from "../../components/ListField";
 import timePng from "../../assets/images/time.png";
 import servingPng from "../../assets/images/serving.png";
-
+import CommentSection from "../../components/CommentSection";
 export default function ViewPost() {
-    const ingredients = ["1 1/2 pounds ground beef","1 egg","1 onion chopped","1 cup milk"];
-    const preparationSteps = ["Mix flour, baking powder, salt, and sugar In a medium-size mixing bowl or large glass measuring cup, whisk together your dry ingredients (or follow directions for premade pancake mix).", 
+  const ingredients = [
+    "1 1/2 pounds ground beef",
+    "1 egg",
+    "1 onion chopped",
+    "1 cup milk"
+  ];
+  const preparationSteps = [
+    "Mix flour, baking powder, salt, and sugar In a medium-size mixing bowl or large glass measuring cup, whisk together your dry ingredients (or follow directions for premade pancake mix).",
     "Mix milk, eggs, and oil In a separate bowl, whisk together the wet ingredients (milk, eggs, vegetable oil, or melted butter) until the egg is broken up (this will prevent overmixing in the next step).",
-     "Mix dry ingredients with wet ingredients Add the wet ingredients to the bowl with the dry ingredients. Stir them"];
-     const addons=[{title:'Preparation',info:'1 h 10 m',image:timePng},{title:'Serving',info:'8 servings',image:servingPng}]
-    return (
-        <div>
-            <ListField recipeInfo={ingredients} type='ingredients_list' name='Ingredients'/>
-            <ListField recipeInfo={preparationSteps} type='htp_list' name='How to Prepare' addons={addons}  numerical/>
-        </div>
-    )
+    "Mix dry ingredients with wet ingredients Add the wet ingredients to the bowl with the dry ingredients. Stir them"
+  ];
+  const addons = [
+    { title: "Preparation", info: "1 h 10 m", image: timePng },
+    { title: "Serving", info: "8 servings", image: servingPng }
+  ];
+  return (
+    <div>
+      <ListField
+        recipeInfo={ingredients}
+        type="ingredients_list"
+        name="Ingredients"
+      />
+      <ListField
+        recipeInfo={preparationSteps}
+        type="htp_list"
+        name="How to Prepare"
+        addons={addons}
+        numerical
+      />
+      <CommentSection />
+    </div>
+  );
 }
