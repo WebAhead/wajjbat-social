5d32ad1a851a8f441b8e79d14b7c8bad9eb80d9f
Fatmeeh <lasheenfatmeeh@gmail.com>
2020-03-29 16:27:13 +0300

    change followers/following link

--- src/pages/Followers/index.jsx
+++ src/pages/Followers/index.jsx
@@ -1,224 +1,56 @@
-import React from 'react';
 import './follow.css';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import MainFooter from '../../components/MainFooter';
+import React from 'react';
+import { BrowserRouter as Router, Link, useLocation } from 'react-router-dom';
 import MainHeader from '../../components/MainHeader';
+import MainFooter from '../../components/MainFooter';
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
+export default function QueryParamsExample() {
   return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </Typography>
+    <Router>
+      <QueryParamsDemo />
+    </Router>
   );
 }
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`
-  };
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
 }
-
-export default function SimpleTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+function QueryParamsDemo() {
+  let query = useQuery();
 
   return (
     <div>
-      {/* <MainHeader /> */}
       <div>
-        <div className="barfollow">
-          <AppBar position="static">
-            <Tabs className="tab" centered value={value} onChange={handleChange} aria-label="simple tabs example">
-              <Tab label="followers" {...a11yProps(0)} />
-              <Tab label="following" {...a11yProps(1)} />
-            </Tabs>
-          </AppBar>
-        </div>
-        <TabPanel className="tappanel" value={value} index={0}>
-          <List className="followers">
-            <ListItem className="followersitem">
-              <ListItemAvatar className="ListItemAvatar">
-                <Avatar
-                  alt="Remy Sharp"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSryt2zIsuLWBGnlbljN25G4eorGzIPJuDxN8EcJkqEUPbRYkZV"
-                />
-              </ListItemAvatar>
-              <div className="item">
-                <ListItemText className="Name" primary="Name" />
-                <div>
-                  <Button variant="contained" className="Deletbutton">
-                    un follow
-                  </Button>
-                </div>
-              </div>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem className="followersitem">
-              <ListItemAvatar className="ListItemAvatar">
-                <Avatar
-                  alt="Remy Sharp"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSryt2zIsuLWBGnlbljN25G4eorGzIPJuDxN8EcJkqEUPbRYkZV"
-                />
-              </ListItemAvatar>
-              <div className="item">
-                <ListItemText className="Name" primary="Name" />
-                <div>
-                  <Button variant="contained" className="Deletbutton">
-                    un follow
-                  </Button>
-                </div>
-              </div>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem className="followersitem">
-              <ListItemAvatar className="ListItemAvatar">
-                <Avatar
-                  alt="Remy Sharp"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSryt2zIsuLWBGnlbljN25G4eorGzIPJuDxN8EcJkqEUPbRYkZV"
-                />
-              </ListItemAvatar>
-              <div className="item">
-                <ListItemText className="Name" primary="Name" />
-                <div>
-                  <Button variant="contained" className="Deletbutton">
-                    un follow
-                  </Button>
-                </div>
-              </div>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem className="followersitem">
-              <ListItemAvatar className="ListItemAvatar">
-                <Avatar
-                  alt="Remy Sharp"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSryt2zIsuLWBGnlbljN25G4eorGzIPJuDxN8EcJkqEUPbRYkZV"
-                />
-              </ListItemAvatar>
-              <div className="item">
-                <ListItemText className="Name" primary="Name" />
-                <div>
-                  <Button variant="contained" className="Deletbutton">
-                    un follow
-                  </Button>
-                </div>
-              </div>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem className="followersitem">
-              <ListItemAvatar className="ListItemAvatar">
-                <Avatar
-                  alt="Remy Sharp"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSryt2zIsuLWBGnlbljN25G4eorGzIPJuDxN8EcJkqEUPbRYkZV"
-                />
-              </ListItemAvatar>
-              <div className="item">
-                <ListItemText className="Name" primary="Name" />
-                <div>
-                  <Button variant="contained" className="Deletbutton">
-                    un follow
-                  </Button>
-                </div>
-              </div>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-          </List>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <List className="following">
-            <ListItem className="followersitem">
-              <ListItemAvatar className="ListItemAvatar">
-                <Avatar alt="Remy Sharp" src="https://www.w3schools.com/bootstrap4/img_avatar6.png" />
-              </ListItemAvatar>
-              <div className="item">
-                <ListItemText className="Name" primary="Name" />
-                <div>
-                  <Button variant="contained" className="Deletbutton">
-                    un follow
-                  </Button>
-                </div>
-              </div>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem className="followersitem">
-              <ListItemAvatar className="ListItemAvatar">
-                <Avatar alt="Remy Sharp" src="https://www.w3schools.com/bootstrap4/img_avatar6.png" />
-              </ListItemAvatar>
-              <div className="item">
-                <ListItemText className="Name" primary="Name" />
-                <div>
-                  <Button variant="contained" className="Deletbutton">
-                    un follow
-                  </Button>
-                </div>
-              </div>
-            </ListItem>
-            <Divider variant="inset" component="li" />
+        <ul className="barfollow">
+          <Link className="Text" to="/followers?name=followers">
+            followers
+          </Link>
 
-            <ListItem className="followersitem">
-              <ListItemAvatar className="ListItemAvatar">
-                <Avatar alt="Remy Sharp" src="https://www.w3schools.com/bootstrap4/img_avatar6.png" />
-              </ListItemAvatar>
-              <div className="item">
-                <ListItemText className="Name" primary="Name" />
-                <div>
-                  <Button variant="contained" className="Deletbutton">
-                    un follow
-                  </Button>
-                </div>
-              </div>
-            </ListItem>
-            <Divider variant="inset" component="li" />
+          <Link className="Text" to="/followers?name=following">
+            following
+          </Link>
+        </ul>
 
-            <ListItem className="followersitem">
-              <ListItemAvatar className="ListItemAvatar">
-                <Avatar alt="Remy Sharp" src="https://www.w3schools.com/bootstrap4/img_avatar6.png" />
-              </ListItemAvatar>
-              <div className="item">
-                <ListItemText className="Name" primary="Name" />
-                <div>
-                  <Button variant="contained" className="Deletbutton">
-                    un follow
-                  </Button>
-                </div>
-              </div>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-          </List>
-        </TabPanel>
+        <Child name={query.get('name')} />
       </div>
       <MainFooter />
+    </div>
+  );
+}
+
+function Child({ name }) {
+  return (
+    <div>
+      {name ? (
+        <h3>
+          The <code>name</code> in the query string is &quot;{name}
+          &quot;
+        </h3>
+      ) : (
+        <h3>
+          The <code>followers</code> in the query string is &quot;followers &quot;
+        </h3>
+      )}
     </div>
   );
 }
